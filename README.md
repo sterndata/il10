4d1f46f217e738273aacf23c8cf6265e0f416ee6
Steven Stern <steve@sterndata.com>
2017-06-09 16:48:20 -0500

    footer credits

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # il10
+
+Designed by Doug Bantz and developed by Steven Stern for
+
+Northshore Indivisible
+
+This theme is available under the GPL for other Indivisble organizations.
--- components/footer/site-info.php
+++ components/footer/site-info.php
@@ -1,5 +1,5 @@
 <div class="site-info">
 	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'il10' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'il10' ), 'WordPress' ); ?></a>
 	<span class="sep"> | </span>
-	<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'il10' ), 'IL10', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+	<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'il10' ), '<a href="https://github.com/sterndata/il10">IL10</a>', '<a href="https://whatagreatwebsite.net/" rel="developer">What A Great Website</a>' ); ?>
 </div><!-- .site-info -->
