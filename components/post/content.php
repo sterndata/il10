e1e32971e75ea92fdabdee43e5b9377b3626f231
Steven Stern <steve@sterndata.com>
2017-06-11 10:40:08 -0500

    remove featured image display from archives

--- components/post/content.php
+++ components/post/content.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php if ( '' != get_the_post_thumbnail() ) : ?>
-	<?php if ( ! is_single() ) { ?>
+	<?php if ( ! is_single() && ! is_archive() ) { ?>
 		<div class="post-thumbnail">
 			<a href="<?php the_permalink(); ?>">
 				<?php the_post_thumbnail( 'il10-featured-image' ); ?>
