021d438f277855bd7c4517c4b1083716c75ad3a3
Steven Stern <steve@sterndata.com>
2017-06-15 12:51:13 -0500

    remove powered by wordpress, increase social media icon size

--- components/footer/site-info.php
+++ components/footer/site-info.php
@@ -1,7 +1,5 @@
 <div class="site-info">
 	&copy; <?php echo date('Y'); ?> North Shore Indivisible
         <span class="sep"> | </span>
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'il10' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'il10' ), 'WordPress' ); ?></a>
-	<span class="sep"> | </span>
 	<?php printf( esc_html__( 'Theme %1$s designed by %2$s and developed by %3$s.', 'il10' ), '<a href="https://github.com/sterndata/il10">IL10</a>', '<a href="http://dougbantz.com">Doug Bantz</a>', '<a href="https://whatagreatwebsite.net/" rel="developer">What A Great Website</a>' ); ?>
 </div><!-- .site-info -->
